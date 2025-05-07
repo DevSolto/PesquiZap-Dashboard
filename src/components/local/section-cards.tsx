74877ce7d79a8736eabc18dfef53c548402fa3fc
santiago souto <souto.santiago@academico.ifpb.edu.br>
2025-05-06 21:58:36 -0300

    prisma

--- src/components/local/section-cards.tsx
+++ src/components/local/section-cards.tsx
@@ -1,7 +1,4 @@
 'use client'
-
-import { TrendingDownIcon, TrendingUpIcon } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { useEffect, useState } from "react";
 import { usePesquisa } from "@/app/context/PesquisaContext";
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,6 @@
-import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export { prisma }
